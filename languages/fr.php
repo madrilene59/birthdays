0ed2a608b19462c57d590457dbce767f42f7f789
madrilene59 <eagle59@gmail.com>
2015-08-22 22:44:39 +0200

    Create fr.php

---
+++ languages/fr.php
@@ -0,0 +1,52 @@
+<?php
+
+return array(
+	// general stuff
+	'birthdays:no_field_configured' => "Aucun champ anniversaire n'est configuré. Merci de vous rapprocher de votre administrateur afin qu'il configure le plugin",
+	'birthdays:more' => "Plus d'anniversaires",
+	'birthdays:none' => "Aucun anniversaire trouvé",
+	
+	// menu
+	// site
+	'birthdays:menu:site:title' => "Anniversaires",
+	// filter
+	'birthdays:filter:upcoming' => "Anniversaires à venir",
+	// owner_block
+	'birthdays:menu:owner_block:group:title' => "Groupe d'anniversaire",
+	
+	// breadcrumb
+	'birthdays:breadcrumb:all' => "Anniversaires",
+	
+	// profile field type
+	'brithdays:profile_field:type' => "Anniversaire",
+	
+	// settings
+	'birthdays:settings:field_selection' => "Champ profil",
+	'birthdays:settings:field_selection:description' => "Merci de selectionner le champ du profil qui contiendra l'anniversaire des utilisateurs.",
+	'birthdays:settings:field_selection:none' => "Désactiver l'affichage des anniversaires",
+	
+	'birthdays:settings:output_format' => "Format de sortie",
+	'birthdays:settings:output_format:description' => "Vous pouvez controler ici le format de sortie des anniversaires.",
+
+	'birthdays:settings:other' => "Other",
+	'birthdays:settings:other:limit_upcoming' => "est ce que 'anniversaire à venir' doit etre limité à 3 mois max d'avance?",
+	
+	// page titles
+	'birthdays:page:day:title' => "Anniversaire le: %s",
+	'birthdays:page:friends:title' => "%Anniversaires des amis de %s",
+	'birthdays:page:upcoming:title' => "Anniversaires à venir",
+	'birthdays:page:group:title' => "Birthdays of %s's members",
+	
+	// sidebar
+	// datepicker
+	'birthdays:sidebar:datepicker:title' => "Afficher les anniversaires sur",
+	
+	// widget
+	'birthdays:widget:title' => "Anniversaires",
+	'birthdays:widget:description' => "Afficher les anniversaires de tous le monde, vos amis et membres du groupe",
+	'birthdays:widget:settings:who_to_show' => "Qui voulez-vous faire apparaitre dans le widget?",
+	
+	// groups
+	'birthdays:groups:options' => "Afficher les anniversaires des membres du groupe",
+	'birthdays:groups:not_enabled' => "La liste des anniversaires n'est pas activé pour ce groupe",
+);
